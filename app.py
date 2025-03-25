dcadfda1e482aa3243b2d716f7333e04aabca74f
phael77 <rapha555lima@gmail.com>
2025-03-25 20:06:37 -0300

    feat: adicionar a api key e geração de resposta

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
 import streamlit as st
 from langchain_openai.chat_models import ChatOpenAI
+
+st.title("🤖💬 LLM Quickstart App")
+
+openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
+
+def generate_response(message):
+    model = ChatOpenAI(temperature=0.5, api_key = openai_api_key)
+    st.info(model.invoke(message))
+
