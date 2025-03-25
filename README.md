df111e23751b866cc287e75c28a9c58fca5fc02c
phael77 <rapha555lima@gmail.com>
2025-03-25 20:17:06 -0300

    criando a view do streamlit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # LLM_App
 Este repositório contém uma aplicação interativa desenvolvida com Streamlit e LangChain, permitindo a geração de texto a partir de prompts fornecidos pelo usuário.
+
+Uma pequena visualização de um protótipo que depois irei fazer mais ajustes e deixar mais responsivo e bonito esse chatzinho
+
+![alt text](image.png)
--- app.py
+++ app.py
@@ -6,6 +6,21 @@ st.title("🤖💬 LLM Quickstart App")
 openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
 
 def generate_response(message):
-    model = ChatOpenAI(temperature=0.5, api_key = openai_api_key)
-    st.info(model.invoke(message))
+    model = ChatOpenAI(model="gpt-4o-mini", temperature=0.5, api_key=openai_api_key)
+    response = model.invoke(message)
+    
+    # Verifica se a resposta possui um atributo 'content' e exibe corretamente
+    if hasattr(response, "content"):
+        st.markdown(f"**Resposta:**\n\n{response.content}")
+    else:
+        st.error("Erro ao processar a resposta.")
 
+with st.form("my_form"):
+    text = st.text_area("Enter your message here:")
+    submitted = st.form_submit_button("Submit")
+    
+    if not openai_api_key.startswith("sk-"):
+        st.warning("Please enter your OpenAI API key!", icon="⚠")
+    
+    if submitted and openai_api_key.startswith("sk-"):
+        generate_response(text)
